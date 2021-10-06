882a01686a8d4e6ed25458c4930dbcebe4123589
Anton Bolotskih <89161424838@yandex.ru>
2021-10-07 01:19:45 +0300

    Initial, WA on 17

---
+++ funcs.cpp
@@ -0,0 +1,64 @@
+//https://contest.yandex.ru/contest/29188/problems/
+
+#include "funcs.h"
+#include <bits/stdc++.h>
+
+/*! @brief First argument, that returns true for given func
+ *
+ * @tparam Func function pointer, lambda, on given vector should have
+ * that form: "___---"
+ * @param func function pointer, lambda, that should accept int as input
+ * @param left Left limit
+ * @param right Right limit
+ * @param tol Tolerance
+ * @return value
+ */
+template<typename Func>
+int indexOfFirst(Func func, int left, int right){
+    while (left<right){
+        int middle = (left+right)/2;
+        if(func(middle)){
+            right = middle;
+        } else {
+            left = middle+1;
+        }
+    }
+    return left;
+}
+
+bool checkPointsCover(int l, int k, const std::vector<int> & v){
+    int i = 0, iMax = v.size()-1;
+    while (k>0){
+        int endPos = v[i]+l;
+        i = indexOfFirst([&endPos, &v](int j){
+            return v[j]>endPos;
+            },i,iMax);
+        if(i>=iMax){
+            if(v[iMax]<=endPos)
+                return true;
+        }
+        --k;
+    }
+    return false;
+}
+
+void parseFile(std::istream & input, std::ostream & output){
+    int n, k;
+    input>>n>>k;
+
+    std::vector<int> numbers(n);
+    std::copy_n(std::istream_iterator<int>(input),n,numbers.begin());
+
+    std::sort(numbers.begin(), numbers.end());
+    int minNumber = numbers[0], maxNumber = numbers.back();
+    int minL = 1, maxL = abs(maxNumber-minNumber-1);
+
+    auto func=[&k, &numbers]
+            (int l){
+        return checkPointsCover(l,k,numbers);
+    };
+
+    int ans = indexOfFirst(func,minL,maxL);
+
+    output<<ans<<'\n';
+}
---
+++ test.cpp
@@ -0,0 +1,85 @@
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_ENABLE_BENCHMARKING
+#include "catch.hpp"
+
+TEST_CASE("Catch operability", "[simple]"){
+    REQUIRE(1+1 == 2);
+}
+
+#include "funcs.h"
+
+TEST_CASE("test 1, file", "[simple]"){
+    std::ofstream inputPrepare;
+    inputPrepare.open ("input.txt", std::ofstream::trunc);
+    inputPrepare<<"6 2\n"
+                  "1 2 3 9 8 7";
+    inputPrepare.close();
+
+    std::ifstream input( "input.txt", std::ofstream::in);
+    std::ofstream output("output.txt", std::ofstream::trunc);
+    parseFile(input,output);
+    input.close();
+    output.close();
+    
+    std::ifstream outputCheck("output.txt", std::ofstream::in);
+    std::stringstream buffer;
+    buffer<<outputCheck.rdbuf();
+    outputCheck.close();
+    REQUIRE(buffer.str() == "2\n");
+}
+/*
+TEST_CASE("file 015", ""){
+    std::ifstream input( "../015", std::ofstream::in);
+    std::stringstream output;
+    parseFile(input,output);
+    input.close();
+    REQUIRE(output.str() == "333333333333333333\n");
+}
+*/
+TEST_CASE("one digit, one line", ""){
+    std::stringstream input, output;
+    input<<"1 1\n"
+           "3";
+    parseFile(input,output);
+    REQUIRE(output.str() == "1\n");
+}
+
+TEST_CASE("one digit, more lines", ""){
+    std::stringstream input, output;
+    input<<"1 123\n"
+           "3";
+    parseFile(input,output);
+    REQUIRE(output.str() == "1\n");
+}
+
+TEST_CASE("one zero digit, more lines", ""){
+    std::stringstream input, output;
+    input<<"1 123\n"
+           "0";
+    parseFile(input,output);
+    REQUIRE(output.str() == "1\n");
+}
+
+TEST_CASE("all negative", ""){
+    std::stringstream input, output;
+    input<<"6 2\n"
+           "-1 -2 -3 -7 -8 -9";
+    parseFile(input,output);
+    REQUIRE(output.str() == "2\n");
+}
+
+TEST_CASE("both signs", ""){
+    std::stringstream input, output;
+    input<<"6 2\n"
+           "-1 -2 -3 7 8 9";
+    parseFile(input,output);
+    REQUIRE(output.str() == "2\n");
+}
+
+TEST_CASE("range", ""){
+    std::stringstream input, output;
+    input<<"6 2\n"
+           "-1000000000 -300000000 -200000000 700000000 800000000 1000000000";
+    parseFile(input,output);
+    REQUIRE(output.str() == "800000000\n");
+}
